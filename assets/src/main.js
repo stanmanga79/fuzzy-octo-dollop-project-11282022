fedc2eb89f7bc4682d7503522ce65878240914ac
louis mane <dennis.itua1@gmail.com>
2022-11-29 19:21:29 -0500

    Updated html with image

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,3 +1,9 @@
+var imageEl = document.getElementById("image");
+
+var gifImage = document.createElement("img");
+gifImage.setAttribute("src", "./assets/images/cat-computer.gif");
+imageEl.appendChild(gifImage);
+
 console.log(`Cool code will be added there later`)
 
 function crystal() {
